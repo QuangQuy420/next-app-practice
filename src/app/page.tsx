f2bc3c19c063d3c7ab5637b496c1595e36fef6cb
Quy Phan Quang <quy.phanquang@thrivelearning.com>
2024-11-17 16:43:57 +0700

    feat: add robots and sitemap file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Head from "next/head";
+
 function Home() {
   return (
       <div>
-        Home Page
+          <Head>
+              <title>Bogotea in Nhat Thien district</title>
+              <meta name="description" content="This is a Bogotea in Nhat Thien district."/>
+              <meta name="robots" content="index, follow"/>
+          </Head>
+          <h1>Welcome Bogotea in Nhat Thien district</h1>
       </div>
   )
 }
