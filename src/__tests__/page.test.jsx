32cf08b92db4c49fb00904e61933a0f2acfda95d
Quy Phan Quang <quy.phanquang@thrivelearning.com>
2024-11-17 13:37:57 +0700

    feat: setup and add unit test

---
+++ src/__tests__/page.test.jsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import Home from '../app/page'
+
+describe('Home', () => {
+    it('renders a home page', () => {
+        render(<Home />)
+
+        expect(screen.getByText('Home Page')).toBeInTheDocument();
+    })
+})
